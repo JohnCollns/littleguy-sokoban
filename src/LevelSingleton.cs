a52f7527f77be9127e2b487e2419f61d694de371
johncEV <john.collins@emptyvessel.io>
2026-08-09 17:48:27 +1000

    Merged in moveable-branch

---
+++ src/LevelSingleton.cs
@@ -0,0 +1,94 @@
+using Godot;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Godot.Collections;
+
+//using Godot.Collections;
+
+public struct MoveRecord
+{
+    public Moveable Moveable;
+    public Vector2I Direction;
+
+    public MoveRecord(Moveable moveable_, Vector2I direction_)
+    {
+        Moveable = moveable_;
+        Direction = direction_;
+    }
+}
+
+public partial class LevelSingleton : Node
+{
+    public static LevelSingleton Instance { get; private set; }
+    private TileMapLayer TileMapLayer;
+    public List<Moveable> Moveables = new List<Moveable>();
+    //private List<MoveRecord[]> MoveHistory = new List<MoveRecord[]>();
+    private List<List<MoveRecord>> MoveHistory = new List<List<MoveRecord>>();
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        LevelSingleton.Instance = this;
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        GetTree().SceneChanged += OnLevelChanged;
+        OnLevelChanged();
+    }
+
+    private void OnLevelChanged()
+    {
+        MoveHistory.Clear();
+        TileMapLayer = (TileMapLayer)GetTree().GetFirstNodeInGroup("LevelTileMap");
+        GD.Print($"OnLevelChanged(), TileMapLayer found: {TileMapLayer != null}");
+    }
+
+    public bool IsTileWall(Vector2I pos)
+    {
+        if (TileMapLayer == null)
+            return false;
+        int posSourceID = TileMapLayer.GetCellSourceId(pos); 
+        return Constants.BlockingTileMapIDs.Contains(posSourceID);
+    }
+
+    public Moveable GetMoveableAtPosition(Vector2I pos)
+    {
+        // Is there a more computationally efficient way of doing this? 
+        foreach (Moveable moveable in Moveables)
+        {
+            if (moveable.tilePos == pos)
+            {
+                return moveable;
+            }
+        }
+        return null;
+    }
+
+    public void StartNewTurn()
+    {
+        MoveHistory.Add(new List<MoveRecord>());
+    }
+
+    public void AddMoveToTurn(Moveable moveable, Vector2I direction)
+    {
+        MoveRecord moveRecord = new MoveRecord(moveable, direction);
+        MoveHistory.Last().Add(moveRecord);
+    }
+
+    public void UndoPreviousTurn()
+    {
+        if (MoveHistory.Count == 0)
+            return;
+
+        var lastMove = MoveHistory.Last();
+        MoveHistory.RemoveAt(MoveHistory.Count - 1);
+        foreach (MoveRecord moveRecord in lastMove)
+        {
+            moveRecord.Moveable.Slide(-moveRecord.Direction);
+        }
+    }
+}
---
+++ src/Moveable.cs
@@ -0,0 +1,73 @@
+using Godot;
+using System;
+
+public partial class Moveable : Node2D
+{
+    public Vector2I tilePos { get; protected set; }
+    protected Tween tween;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        DetermineTilePos();
+    }
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        LevelSingleton.Instance.Moveables.Add(this);
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        LevelSingleton.Instance.Moveables.Remove(this);
+    }
+
+    public void ApplyTilePos()
+    {
+        Position = (tilePos * Constants.GridSize) + Constants.HalfGridOffset;
+    }
+    public void DetermineTilePos()
+    {
+        tilePos = MathsHelper.Vector2IFromVector2((Position - Constants.HalfGridOffset) / (float)Constants.GridSize);
+    }
+    public Vector2 PredictTilePos(Vector2I direction)
+    {
+        return ((tilePos + direction) * Constants.GridSize) + Constants.HalfGridOffset;
+    }
+
+    public virtual bool CanMove(Vector2I direction)
+    {
+        if (LevelSingleton.Instance.IsTileWall(tilePos + direction))
+            return false;
+        
+        // Recursively check if this would push a moveable, and if that moveable can move in the direction. 
+        if (LevelSingleton.Instance.GetMoveableAtPosition(tilePos + direction) is Moveable moveableToPush)
+        {
+            return moveableToPush.CanMove(direction);
+        }
+        return true;
+    }
+
+    public virtual void Move(Vector2I direction)
+    {
+        if (LevelSingleton.Instance.GetMoveableAtPosition(tilePos + direction) is Moveable moveableToPush)
+        {
+            moveableToPush.Move(direction);
+        }
+        Slide(direction);
+        LevelSingleton.Instance.AddMoveToTurn(this, direction);
+    }
+
+    public void Slide(Vector2I Direction)
+    {
+        ApplyTilePos();
+        Vector2 target = PredictTilePos(Direction);
+        tilePos += Direction;
+
+        tween = CreateTween();
+        tween.TweenProperty(this, "position", target, Constants.MoveDuration);
+        //GD.Print($"Slide, tweening from: {tilePos} => {Position}, to: {target}");
+    }
+}
