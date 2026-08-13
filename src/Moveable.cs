58931bf2b0b8ff2c4c86fc399c38e25c670139ef
ricepaddyhat <matt.gelabert@gmail.com>
2026-08-13 13:06:16 +1000

    Adding dialogue to boss transition

--- src/Moveable.cs
+++ src/Moveable.cs
@@ -3,119 +3,119 @@ using System;
 
 public partial class Moveable : Node2D
 {
-    public Vector2I tilePos { get; set; }
-    [Export] public EBlockType BlockType { get; protected set; }
-    protected Tween tween;
+	public Vector2I tilePos { get; set; }
+	[Export] public EBlockType BlockType { get; protected set; }
+	protected Tween tween;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        DetermineTilePos();
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		DetermineTilePos();
+	}
 
-    public override void _EnterTree()
-    {
-        base._EnterTree();
-        LevelSingleton.Instance.Moveables.Add(this);
-    }
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		LevelSingleton.Instance.Moveables.Add(this);
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-        LevelSingleton.Instance.Moveables.Remove(this);
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		LevelSingleton.Instance.Moveables.Remove(this);
+	}
 
-    public void ApplyTilePos()
-    {
-        Position = (tilePos * Constants.GridSize) + Constants.HalfGridOffset;
-    }
-    public void DetermineTilePos()
-    {
-        tilePos = MathsHelper.Vector2IFromVector2((Position - Constants.HalfGridOffset) / (float)Constants.GridSize);
-    }
-    public Vector2 PredictTilePos(Vector2I direction)
-    {
-        return ((tilePos + direction) * Constants.GridSize) + Constants.HalfGridOffset;
-    }
+	public void ApplyTilePos()
+	{
+		Position = (tilePos * Constants.GridSize) + Constants.HalfGridOffset;
+	}
+	public void DetermineTilePos()
+	{
+		tilePos = MathsHelper.Vector2IFromVector2((Position - Constants.HalfGridOffset) / (float)Constants.GridSize);
+	}
+	public Vector2 PredictTilePos(Vector2I direction)
+	{
+		return ((tilePos + direction) * Constants.GridSize) + Constants.HalfGridOffset;
+	}
 
-    public virtual bool CanMove(Vector2I direction)
-    {
-        return CanMoveStatic(tilePos, direction, this);
-    }
+	public virtual bool CanMove(Vector2I direction)
+	{
+		return CanMoveStatic(tilePos, direction, this);
+	}
 
-    // Should rename this function. 
-    // This is just a static version of CanMove(), it uses any location and then sees if its move is valid. 
-    public static bool CanMoveStatic(Vector2I tilePos, Vector2I direction, Moveable sourceMoveable)
-    {
-        if (LevelSingleton.Instance.IsTileWall(tilePos + direction))
-            return false;
-        
-        // Recursively check if this would push a moveable, and if that moveable can move in the direction. 
-        if (LevelSingleton.Instance.GetMoveableAtPosition(tilePos + direction) is Moveable moveableToPush)
-        {
-            GD.Print($"Querying moveable at: {moveableToPush.tilePos}");
-            if (sourceMoveable == moveableToPush)
-            {
-                return true;
-            }
-            return moveableToPush.CanMove(direction);
-        }
-        return true;
-    }
+	// Should rename this function. 
+	// This is just a static version of CanMove(), it uses any location and then sees if its move is valid. 
+	public static bool CanMoveStatic(Vector2I tilePos, Vector2I direction, Moveable sourceMoveable)
+	{
+		if (LevelSingleton.Instance.IsTileWall(tilePos + direction))
+			return false;
+		
+		// Recursively check if this would push a moveable, and if that moveable can move in the direction. 
+		if (LevelSingleton.Instance.GetMoveableAtPosition(tilePos + direction) is Moveable moveableToPush)
+		{
+			GD.Print($"Querying moveable at: {moveableToPush.tilePos}");
+			if (sourceMoveable == moveableToPush)
+			{
+				return true;
+			}
+			return moveableToPush.CanMove(direction);
+		}
+		return true;
+	}
 
-    public virtual bool DoesOccupyPosition(Vector2I position)
-    {
-        return position == tilePos;
-    }
+	public virtual bool DoesOccupyPosition(Vector2I position)
+	{
+		return position == tilePos;
+	}
 
-    public virtual void Move(Vector2I direction)
-    {
-        if (LevelSingleton.Instance.GetMoveableAtPosition(tilePos + direction) is Moveable moveableToPush)
-        {
-            if (moveableToPush.BlockType == EBlockType.Orb && BlockType != EBlockType.Bouncer)
-            {
-                GD.Print($"Block of type: {BlockType} moved into orb, destroying self");
-                moveableToPush.Destroy();
-                Destroy();
-            }
-            if (moveableToPush != this)
-            {
-                moveableToPush.Move(direction);
-            }
-        }
-        Slide(direction);
-        LevelSingleton.Instance.AddMoveToTurn(this, direction);
-    }
+	public virtual void Move(Vector2I direction)
+	{
+		if (LevelSingleton.Instance.GetMoveableAtPosition(tilePos + direction) is Moveable moveableToPush)
+		{
+			if (moveableToPush.BlockType == EBlockType.Orb && BlockType != EBlockType.Bouncer)
+			{
+				GD.Print($"Block of type: {BlockType} moved into orb, destroying self");
+				moveableToPush.Destroy();
+				Destroy();
+			}
+			if (moveableToPush != this)
+			{
+				moveableToPush.Move(direction);
+			}
+		}
+		Slide(direction);
+		LevelSingleton.Instance.AddMoveToTurn(this, direction);
+	}
 
-    public virtual void Slide(Vector2I Direction)
-    {
-        ApplyTilePos();
-        Vector2 target = PredictTilePos(Direction);
-        tilePos += Direction;
+	public virtual void Slide(Vector2I Direction)
+	{
+		ApplyTilePos();
+		Vector2 target = PredictTilePos(Direction);
+		tilePos += Direction;
 
-        tween = CreateTween();
-        tween.TweenProperty(this, "position", target, Constants.MoveDuration);
-        //GD.Print($"Slide, tweening from: {tilePos} => {Position}, to: {target}");
-    }
+		tween = CreateTween();
+		tween.TweenProperty(this, "position", target, Constants.MoveDuration);
+		//GD.Print($"Slide, tweening from: {tilePos} => {Position}, to: {target}");
+	}
 
-    public virtual bool IsInGoal()
-    {
-        return LevelSingleton.Instance.IsTileGoal(tilePos);
-    }
+	public virtual bool IsInGoal()
+	{
+		return LevelSingleton.Instance.IsTileGoal(tilePos);
+	}
 
-    public bool IsLittleGuy()
-    {
-        return Constants.Friends.Contains(BlockType);
-    }
+	public bool IsLittleGuy()
+	{
+		return Constants.Friends.Contains(BlockType);
+	}
 
-    public virtual void Destroy()
-    {
-        // don't know what to put
-        if (Constants.Friends.Contains(BlockType))
-        {
-            LevelSingleton.Instance.EmitFriendDamaged();
-        }
+	public virtual void Destroy()
+	{
+		// don't know what to put
+		if (Constants.Friends.Contains(BlockType))
+		{
+			LevelSingleton.Instance.EmitFriendDamaged();
+		}
 
-        QueueFree();
-    }
+		QueueFree();
+	}
 }
--- src/SpeechBubble.cs
+++ src/SpeechBubble.cs
@@ -2,206 +2,206 @@ using Godot;
 
 public partial class SpeechBubble : Node2D
 {
-    // =========================================================================
-    // DIALOGUE SETTINGS
-    // =========================================================================
-    [ExportGroup("Dialogue Settings")]
-    [Export] private string[] _chatLines = new string[]
-    {
-        "Mein Gott Leute",
-        "Meine Mama hat mir einfach erlaubt dass ich Cola trinken darf!",
-        "Wie cool ist das bitte?",
-        "Jetzt zocke ich Fortnite und trinke Cola!",
+	// =========================================================================
+	// DIALOGUE SETTINGS
+	// =========================================================================
+	[ExportGroup("Dialogue Settings")]
+	[Export] private string[] _chatLines = new string[]
+	{
+		"Mein Gott Leute",
+		"Meine Mama hat mir einfach erlaubt dass ich Cola trinken darf!",
+		"Wie cool ist das bitte?",
+		"Jetzt zocke ich Fortnite und trinke Cola!",
         "YIPPEE!"
-    };
+	};
 
-    [Export] private bool _autoStartTimer = true;
-    [Export] private float _displayDuration = 3.0f; // Time bubble stays visible
+	[Export] private bool _autoStartTimer = true;
+	[Export] private float _displayDuration = 3.0f; // Time bubble stays visible
 
-    [ExportSubgroup("Timers")]
-    // Initial delay before first line triggers (Wider stagger window)
-    [Export] private float _initialMinDelay = 3.0f;    
-    [Export] private float _initialMaxDelay = 15.0f;   
-    
-    // Cooldown interval between lines (9 to 20 seconds)
-    [Export] private float _minChatInterval = 9.0f;    
-    [Export] private float _maxChatInterval = 20.0f;   
+	[ExportSubgroup("Timers")]
+	// Initial delay before first line triggers (Wider stagger window)
+	[Export] private float _initialMinDelay = 3.0f;    
+	[Export] private float _initialMaxDelay = 15.0f;   
+	
+	// Cooldown interval between lines (9 to 20 seconds)
+	[Export] private float _minChatInterval = 9.0f;    
+	[Export] private float _maxChatInterval = 20.0f;   
 
-    // =========================================================================
-    // VISUAL CUSTOMIZATION & STYLING
-    // =========================================================================
-    [ExportGroup("Visual Customization")]
-    [Export] private Vector2 _bubbleOffset = new Vector2(0, -165);
-    [Export] private int _fontSize = 72;
-    [Export] private float _maxWidth = 750f;
+	// =========================================================================
+	// VISUAL CUSTOMIZATION & STYLING
+	// =========================================================================
+	[ExportGroup("Visual Customization")]
+	[Export] private Vector2 _bubbleOffset = new Vector2(0, -165);
+	[Export] private int _fontSize = 72;
+	[Export] private float _maxWidth = 750f;
 
-    // Default Palette: Soft Cream + Deep Slate Indigo (Thicker 8px Border)
-    [Export] private Color _backgroundColor = new Color("FFFDF0"); 
-    [Export] private Color _textColor = new Color("1E1E38");       
-    [Export] private Color _borderColor = new Color("1E1E38");     
-    [Export] private int _borderWidth = 8;                         
-    [Export] private int _cornerRadius = 22;
+	// Default Palette: Soft Cream + Deep Slate Indigo (Thicker 8px Border)
+	[Export] private Color _backgroundColor = new Color("FFFDF0"); 
+	[Export] private Color _textColor = new Color("1E1E38");       
+	[Export] private Color _borderColor = new Color("1E1E38");     
+	[Export] private int _borderWidth = 8;                         
+	[Export] private int _cornerRadius = 22;
 
-    // =========================================================================
-    // PRIVATE VARIABLES
-    // =========================================================================
-    private Label _label;
-    private PanelContainer _panel;
-    private Node2D _parent2D;
-    private Timer _loopTimer;
-    private Timer _hideTimer;
+	// =========================================================================
+	// PRIVATE VARIABLES
+	// =========================================================================
+	private Label _label;
+	private PanelContainer _panel;
+	private Node2D _parent2D;
+	private Timer _loopTimer;
+	private Timer _hideTimer;
 
-    public override void _Ready()
-    {
-        _parent2D = GetParent<Node2D>();
-        if (_parent2D == null)
-        {
-            GD.PrintErr("[SpeechBubble] ERROR: Parent node is null!");
-            return;
-        }
+	public override void _Ready()
+	{
+		_parent2D = GetParent<Node2D>();
+		if (_parent2D == null)
+		{
+			GD.PrintErr("[SpeechBubble] ERROR: Parent node is null!");
+			return;
+		}
 
-        TopLevel = true;
-        ZIndex = 100;
+		TopLevel = true;
+		ZIndex = 100;
 
-        BuildBubbleUI();
+		BuildBubbleUI();
 
-        _loopTimer = new Timer { OneShot = true };
-        _loopTimer.Timeout += OnLoopTimeout;
-        AddChild(_loopTimer);
+		_loopTimer = new Timer { OneShot = true };
+		_loopTimer.Timeout += OnLoopTimeout;
+		AddChild(_loopTimer);
 
-        _hideTimer = new Timer { OneShot = true };
-        _hideTimer.Timeout += Clear;
-        AddChild(_hideTimer);
+		_hideTimer = new Timer { OneShot = true };
+		_hideTimer.Timeout += Clear;
+		AddChild(_hideTimer);
 
-        Clear();
+		Clear();
 
-        if (_autoStartTimer)
-        {
-            // First line picks a random time between 3.0 and 15.0 seconds
-            float initialWait = (float)GD.RandRange(_initialMinDelay, _initialMaxDelay);
-            _loopTimer.Start(initialWait);
-        }
-    }
+		if (_autoStartTimer)
+		{
+			// First line picks a random time between 3.0 and 15.0 seconds
+			float initialWait = (float)GD.RandRange(_initialMinDelay, _initialMaxDelay);
+			_loopTimer.Start(initialWait);
+		}
+	}
 
-    public override void _Process(double delta)
-    {
-        if (_parent2D == null || !GodotObject.IsInstanceValid(_parent2D))
-            return;
+	public override void _Process(double delta)
+	{
+		if (_parent2D == null || !GodotObject.IsInstanceValid(_parent2D))
+			return;
 
-        GlobalPosition = _parent2D.GlobalPosition + _bubbleOffset;
-    }
+		GlobalPosition = _parent2D.GlobalPosition + _bubbleOffset;
+	}
 
-    // =========================================================================
-    // PUBLIC API
-    // =========================================================================
+	// =========================================================================
+	// PUBLIC API
+	// =========================================================================
 
-    public void Say(string text)
-    {
-        if (string.IsNullOrEmpty(text) || _label == null || _panel == null) return;
+	public void Say(string text)
+	{
+		if (string.IsNullOrEmpty(text) || _label == null || _panel == null) return;
 
-        _label.Text = text;
-        _panel.Visible = true;
+		_label.Text = text;
+		_panel.Visible = true;
 
-        Font font = _label.GetThemeFont("font") ?? ThemeDB.FallbackFont;
+		Font font = _label.GetThemeFont("font") ?? ThemeDB.FallbackFont;
 
-        Vector2 singleLineSize = font.GetStringSize(text, HorizontalAlignment.Left, -1, _fontSize);
-        Vector2 exactTextSize;
+		Vector2 singleLineSize = font.GetStringSize(text, HorizontalAlignment.Left, -1, _fontSize);
+		Vector2 exactTextSize;
 
-        if (singleLineSize.X <= _maxWidth)
-        {
-            _label.AutowrapMode = TextServer.AutowrapMode.Off;
-            exactTextSize = singleLineSize;
-        }
-        else
-        {
-            _label.AutowrapMode = TextServer.AutowrapMode.WordSmart;
-            exactTextSize = font.GetMultilineStringSize(text, HorizontalAlignment.Center, _maxWidth, _fontSize);
-        }
+		if (singleLineSize.X <= _maxWidth)
+		{
+			_label.AutowrapMode = TextServer.AutowrapMode.Off;
+			exactTextSize = singleLineSize;
+		}
+		else
+		{
+			_label.AutowrapMode = TextServer.AutowrapMode.WordSmart;
+			exactTextSize = font.GetMultilineStringSize(text, HorizontalAlignment.Center, _maxWidth, _fontSize);
+		}
 
-        _label.CustomMinimumSize = exactTextSize;
-        CallDeferred(nameof(UpdatePanelAlignment));
+		_label.CustomMinimumSize = exactTextSize;
+		CallDeferred(nameof(UpdatePanelAlignment));
 
-        float duration = _displayDuration > 0 ? _displayDuration : 3.0f;
-        _hideTimer.Start(duration);
-    }
+		float duration = _displayDuration > 0 ? _displayDuration : 3.0f;
+		_hideTimer.Start(duration);
+	}
 
-    public void SayRandom()
-    {
-        string[] lines = (_chatLines != null && _chatLines.Length > 0) 
-            ? _chatLines 
-            : new string[] { "YIPPEE!", "Cola!" };
+	public void SayRandom()
+	{
+		string[] lines = (_chatLines != null && _chatLines.Length > 0) 
+			? _chatLines 
+			: new string[] { "YIPPEE!", "Cola!" };
 
-        int index = GD.RandRange(0, lines.Length - 1);
-        Say(lines[index]);
-    }
+		int index = GD.RandRange(0, lines.Length - 1);
+		Say(lines[index]);
+	}
 
-    public void Clear()
-    {
-        if (_panel != null) _panel.Visible = false;
-    }
+	public void Clear()
+	{
+		if (_panel != null) _panel.Visible = false;
+	}
 
-    // =========================================================================
-    // PRIVATE HELPERS
-    // =========================================================================
+	// =========================================================================
+	// PRIVATE HELPERS
+	// =========================================================================
 
-    private void OnLoopTimeout()
-    {
-        SayRandom();
-        StartNextCycleTimer();
-    }
+	private void OnLoopTimeout()
+	{
+		SayRandom();
+		StartNextCycleTimer();
+	}
 
-    private void StartNextCycleTimer()
-    {
-        if (_loopTimer == null) return;
+	private void StartNextCycleTimer()
+	{
+		if (_loopTimer == null) return;
 
-        // Subsequent lines pick a random cooldown between 9.0 and 20.0 seconds
-        float min = _minChatInterval > 0 ? _minChatInterval : 9.0f;
-        float max = _maxChatInterval >= min ? _maxChatInterval : min + 11.0f;
+		// Subsequent lines pick a random cooldown between 9.0 and 20.0 seconds
+		float min = _minChatInterval > 0 ? _minChatInterval : 9.0f;
+		float max = _maxChatInterval >= min ? _maxChatInterval : min + 11.0f;
 
-        float wait = (float)GD.RandRange(min, max);
-        _loopTimer.Start(wait);
-    }
+		float wait = (float)GD.RandRange(min, max);
+		_loopTimer.Start(wait);
+	}
 
-    private void BuildBubbleUI()
-    {
-        _panel = new PanelContainer();
+	private void BuildBubbleUI()
+	{
+		_panel = new PanelContainer();
 
-        var styleBox = new StyleBoxFlat
-        {
-            BgColor = _backgroundColor,
-            CornerRadiusTopLeft = _cornerRadius,
-            CornerRadiusTopRight = _cornerRadius,
-            CornerRadiusBottomLeft = _cornerRadius,
-            CornerRadiusBottomRight = _cornerRadius,
+		var styleBox = new StyleBoxFlat
+		{
+			BgColor = _backgroundColor,
+			CornerRadiusTopLeft = _cornerRadius,
+			CornerRadiusTopRight = _cornerRadius,
+			CornerRadiusBottomLeft = _cornerRadius,
+			CornerRadiusBottomRight = _cornerRadius,
 
-            BorderWidthLeft = _borderWidth,
-            BorderWidthTop = _borderWidth,
-            BorderWidthRight = _borderWidth,
-            BorderWidthBottom = _borderWidth,
-            BorderColor = _borderColor,
+			BorderWidthLeft = _borderWidth,
+			BorderWidthTop = _borderWidth,
+			BorderWidthRight = _borderWidth,
+			BorderWidthBottom = _borderWidth,
+			BorderColor = _borderColor,
 
-            ContentMarginLeft = 32,
-            ContentMarginRight = 32,
-            ContentMarginTop = 18,
-            ContentMarginBottom = 18
-        };
-        _panel.AddThemeStyleboxOverride("panel", styleBox);
+			ContentMarginLeft = 32,
+			ContentMarginRight = 32,
+			ContentMarginTop = 18,
+			ContentMarginBottom = 18
+		};
+		_panel.AddThemeStyleboxOverride("panel", styleBox);
 
-        _label = new Label();
-        _label.AddThemeColorOverride("font_color", _textColor);
-        _label.AddThemeFontSizeOverride("font_size", _fontSize);
-        _label.HorizontalAlignment = HorizontalAlignment.Center;
-        _label.VerticalAlignment = VerticalAlignment.Center;
+		_label = new Label();
+		_label.AddThemeColorOverride("font_color", _textColor);
+		_label.AddThemeFontSizeOverride("font_size", _fontSize);
+		_label.HorizontalAlignment = HorizontalAlignment.Center;
+		_label.VerticalAlignment = VerticalAlignment.Center;
 
-        _panel.AddChild(_label);
-        AddChild(_panel);
-    }
+		_panel.AddChild(_label);
+		AddChild(_panel);
+	}
 
-    private void UpdatePanelAlignment()
-    {
-        if (_panel == null) return;
+	private void UpdatePanelAlignment()
+	{
+		if (_panel == null) return;
 
-        _panel.ResetSize();
-        _panel.Position = new Vector2(-_panel.Size.X / 2.0f, -_panel.Size.Y);
-    }
+		_panel.ResetSize();
+		_panel.Position = new Vector2(-_panel.Size.X / 2.0f, -_panel.Size.Y);
+	}
 }
